f652bfb181a3bf50be47c7fe38c1862917910477
Inna-Mykytiuk <innka-pinnka@ukr.net>
2024-09-01 14:54:15 +0300

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -18,37 +18,46 @@ const App: React.FC = () => {
   };
 
   return (
-    <div className="App">
-      <h1>User Management Table</h1>
-      <input
-        type="text"
-        placeholder="Search by name, username, email, or phone..."
-        onChange={handleSearch}
-        className="search-input"
-      />
-      {loading && <p>Loading...</p>}
-      {error && <p>Error: {error}</p>}
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Username</th>
-            <th>Email</th>
-            <th>Phone</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredUsers.map((user) => (
-            <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{user.username}</td>
-              <td>{user.email}</td>
-              <td>{user.phone}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <section className="section">
+      <div className='container'>
+        <div className='wrapper'>
+          <div className='hero'>
+            <img src="https://images.unsplash.com/photo-1513128034602-7814ccaddd4e?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="hero image" className='hero-img' />
+          </div>
+          <div className='table-container'>
+            <h1 className='title'>User Management Table</h1>
+            <input
+              type="text"
+              placeholder="Search by name, username, email, or phone..."
+              onChange={handleSearch}
+              className="search-input"
+            />
+            {loading && <p>Loading...</p>}
+            {error && <p>Error: {error}</p>}
+            <table>
+              <thead>
+                <tr className="table-header">
+                  <th>Name</th>
+                  <th>Username</th>
+                  <th>Email</th>
+                  <th>Phone</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredUsers.map((user) => (
+                  <tr key={user.id}>
+                    <td className="user-name">{user.name}</td>
+                    <td>{user.username}</td>
+                    <td>{user.email}</td>
+                    <td className='user-phone'>{user.phone}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
